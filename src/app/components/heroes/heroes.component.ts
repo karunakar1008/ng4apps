3d8f00ae2effbc3ab04ffbcabb3139d5c00d8c32
karunakar <karunakar1008@gmail.com>
2017-11-12 22:24:52 +0530

    Added click event listener in component for li cick event

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -12,11 +12,13 @@ export class HeroesComponent implements OnInit {
 
   constructor() { }
 
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
   heroes = HEROES;
+ 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   ngOnInit() {
   }
 
