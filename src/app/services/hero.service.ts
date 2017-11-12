df98e2633b0cff97c420a2aa44211e61068fb7da
karunakar <karunakar1008@gmail.com>
2017-11-12 23:17:57 +0530

    added npm install rxjs to suppoer async http return data

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../models/hero';
 import { HEROES } from './mock-heroes.service';
+import { Observable } from 'rxjs/Observable'; // npm install rxjs
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class HeroService {
