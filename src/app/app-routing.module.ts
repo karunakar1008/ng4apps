d2c740e7864fd47c0a083269e8c5428195a588b2
karunakar <karunakar1008@gmail.com>
2017-11-12 23:47:23 +0530

    Imported route required libraries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: []
 })
