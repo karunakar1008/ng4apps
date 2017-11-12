ef334990ecda75a5342f8de3fa8732d627afa7fe
karunakar <karunakar1008@gmail.com>
2017-11-12 23:37:12 +0530

    inject message service to hero service and message service to Messages component

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,13 +3,17 @@ import { Hero } from '../models/hero';
 import { HEROES } from './mock-heroes.service';
 import { Observable } from 'rxjs/Observable'; // npm install rxjs
 import { of } from 'rxjs/observable/of';
-
+import { MessageService } from './message.service';
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) {
+
+  }
 
   getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
