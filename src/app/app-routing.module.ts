2a7d44222fed4194c387dcb2445c3ce54b2e2531
karunakar <karunakar1008@gmail.com>
2017-11-13 00:12:47 +0530

    Added dashboard to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from '../app/components/heroes/heroes.component';
 import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
+import { HeroDetailComponent } from '../app/components/hero-detail/hero-detail.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
 ];
 
 @NgModule({
